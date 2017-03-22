5845bc126e0b45582eaa53d9afe060a83c7b4000
Travis Check <travis-ci@example.com>
2017-03-21 17:52:25 -0700

    making check a synchronous IIFE

--- test/test.js
+++ test/test.js
@@ -3,8 +3,11 @@ const expect = require('chai').expect
 
 describe('no-master-commits', function () {
   it('check local master branch', function () {
-    expect(function () {
+    try {
       require('../index')
-    }).to.throw(/Commits to the master are not permitted/)
+      expect(true).to.be.false('Check to prevent commits to master branch failed.')
+    } catch (err) {
+      expect(err.message).to.match(/Commits to master branch are not permitted. Create a pull request./)
+    }
   })
 })
