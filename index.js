c4123e894c0f76811dd04e5f7576902bb4c61414
Travis Check <travis-ci@example.com>
2017-03-27 11:56:58 -0700

    better message

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ module.exports = (function () {
   if (!actual) {
     throw (new Error(`${basedir} does not appear to be a git repository`))
   } else if (actual === desired) {
-    const str = chalk.yellow(
-      ` MEMORY AID: to create a new branch:\n` +
-      ` git checkout -b feature-branch-RENAME_ME master\n` +
-      ` git push -u orign feature-branch-RENAME_ME\n`
-    )
+    const c = /^win/.test(process.platform) ? 'rem' : '#'
+    const str = chalk.red(
+      ` Commits to ${desired} branch are not permitted.\n` +
+      ` Please create a branch and then generate a pull-request\n`) +
+      ` ---- \n` +
+      chalk.yellow(
+      ` MEMORY AID: create a new branch\n` +
+      ` git checkout -b feature-branch-:RENAME_ME: master  ${c} (local)\n` +
+      ` git push -u origin feature-branch-:RENAME_ME:      ${c} (remote)\n`) +
+      ` ---- \n`
     console.log(str)
     throw (new Error(`Commits to ${desired} branch are not permitted. Create a pull request.`))
   }
